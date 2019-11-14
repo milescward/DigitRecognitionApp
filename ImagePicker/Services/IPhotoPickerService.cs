c56e71e2db20909a75d74c60c2a1463557f46f05
miles.c.ward@gmail.com <miles.c.ward@gmail.com>
2019-11-14 14:54:57 -0800

    Initial Commit

---
+++ ImagePicker/Services/IPhotoPickerService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ImagePicker.Services
+{
+    public interface IPhotoPickerService
+    {
+        Task<Stream> GetImageStreamAsync();
+    }
+}
---
+++ ImagePicker/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ImagePicker.Models;
+
+namespace ImagePicker.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
